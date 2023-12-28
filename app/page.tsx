6a87175906ae68bca71226b628bca2285237dfd9
Mikolaj <m.lewandowski.0231@gmail.com>
2023-12-28 20:31:25 +0100

    feat: created home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      home
+    <main className=" w-screen h-screen bg-black flex justify-center flex-col items-center text-white ">
+      <div className="w-full max-w-[600px] mx-auto flex flex-col gap-5">
+        <h1 className="text-6xl">The best Journal app</h1>
+        <p className="text-2xl text-white/60">
+          This is the best app for tracking your mood through out your life. All
+          you have to do is be honest
+        </p>
+        <Link href={'/journal'}>
+          <button className="bg-blue-600  px-4 py-3 rounded-lg text-xl cursor-pointer hover:bg-blue-800 duration-500">
+            Get started
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
